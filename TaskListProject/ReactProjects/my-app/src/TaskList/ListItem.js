a72e9765fbedbd9a8f15565cf3081936ee7defb1
Jonah Pena <jonahrpena@gmail.com>
2023-03-17 14:08:03 -0500

    fixed text overflow bugs in styling

--- TaskListProject/ReactProjects/my-app/src/TaskList/ListItem.js
+++ TaskListProject/ReactProjects/my-app/src/TaskList/ListItem.js
@@ -3,25 +3,28 @@ import "./TaskList.css";
 
 function ListItem({ item, index, handleCompleteItem, handleRemoveItem }) {
   return (
-    <li className="list-item">
-      <div className="list-item-inner">
-        <label>
-          <input
-            type="checkbox"
-            onChange={() => handleCompleteItem(item, index)}
-            checked={false}
-          />
-          <span></span>
-        </label>
+    <li>
+      <div className="list-item">
+        <div className="list-item-inner">
+          <label>
+            <input
+              type="checkbox"
+              onChange={() => handleCompleteItem(item, index)}
+              checked={false}
+            />
+            <span></span>
+          </label>
+        </div>
+        <div className="list-item-text">{item.title}</div>
+        <div
+          className="material-icons incomplete-delete-icon"
+          onClick={() => handleRemoveItem(item.id)}
+        >
+          close
+        </div>{" "}
       </div>
-      <div className="list-item-text">{item.title}</div>
+
       <div></div>
-      <span
-        className="material-icons delete-icon"
-        onClick={() => handleRemoveItem(item.id)}
-      >
-        close
-      </span>
     </li>
   );
 }
